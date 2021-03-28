cf95181d290748b4ed97472166c943d00445068e
zoom.zen <yahyawessam.2002@gmail.com>
2021-03-28 15:48:13 +0300

    Dababy-ification

--- index.js
+++ index.js
@@ -17,30 +17,30 @@ export default class amogus extends Plugin {
   start () {
       const Message = getModule(m => m.type && m.type.displayName === 'MessageContent')
       console.log(Message)
-      patch('amoguspng', Message, 'type', (args, res) => {
+      patch('dababypng', Message, 'type', (args, res) => {
       const img = document.getElementsByTagName("img")
       for (let i = 0; i < img.length; i++) {
         const element = img[i];
-        element.setAttribute("src", "https://cdn.discordapp.com/attachments/735710639410118817/815652065749368883/amogus.png")
+        element.setAttribute("src", "https://cdn.discordapp.com/attachments/803993444917051404/825696810307289138/dababy.png")
       }
       const video = document.getElementsByTagName("video")
       for (let i = 0; i < video.length; i++) {
         const element = video[i];
-        if (element.getAttribute("src") != "https://cdn.discordapp.com/attachments/773508852247035915/812451604346830868/when_IMPORSTOR1.mp4") {
-        element.setAttribute("src", "https://cdn.discordapp.com/attachments/773508852247035915/812451604346830868/when_IMPORSTOR1.mp4")
+        if (element.getAttribute("src") != "https://cdn.discordapp.com/attachments/803993444917051404/825697846435119124/Dababy_car.mp4") {
+        element.setAttribute("src", "https://cdn.discordapp.com/attachments/803993444917051404/825697846435119124/Dababy_car.mp4")
         }
       }
-        res.props.children[0] = "amogus" 
+        res.props.children[0] = "lesgooo" 
         const username = document.getElementsByClassName("username-1A8OIy")
         for (let i = 0; i < username.length; i++) {
           const element = username[i];
-          element.innerHTML = "amogus"
+          element.innerHTML = "DaBaby"
         }
         return res;
       })
   }
 
   stop () {
-    unpatch('amoguspng')
+    unpatch('dababypng')
   }
 }
