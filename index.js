5e8f94547e39870eea497e4af454c8edfa6cb53b
Yahya Wessam <yahyawessam2002@gmail.com>
2021-05-15 15:29:23 +0300

    Full and proper frommating (i hope)

--- index.js
+++ index.js
@@ -14,37 +14,38 @@ import { patch, unpatch } from "@vizality/patcher"
 
 
 export default class dababy extends Plugin {
-  start () {
+  start() {
     $vz.commands.registerCommand({
       command: "dababy",
       description: "lesgooo",
-      executor: () => {return {
-        send: false,
-        result: {
-         const :Toast = vizality.api.notifications.sendToast({
-  header: 'DaBaby',
-  content: 'I will turn a discord rebrand into a convertibile',
-  icon: {
-    url: 'https://cdn.discordapp.com/attachments/803993444917051404/842774092411174942/convertibile.png',
-    size: '512'
-  },
-  showCloseButton: false,
-  autoClose: false,
-  buttons: [
-    {
-      text: 'lesgooo',
-      color: 'green'
-    }
-  ]
-}),
+      executor: () => {
+        return {
+          send: false,
+          result: {
+            const: Toast = vizality.api.notifications.sendToast({
+              header: 'DaBaby',
+              content: 'I will turn a discord rebrand into a convertibile',
+              icon: {
+                url: 'https://cdn.discordapp.com/attachments/803993444917051404/842774092411174942/convertibile.png',
+                size: '512'
+              },
+              showCloseButton: false,
+              autoClose: false,
+              buttons: [
+                {
+                  text: 'lesgooo',
+                  color: 'green'
+                }
+              ]
+            }),
+          }
         }
       }
-    }
-  })
+    })
 
-      const Message = getModule(m => m.type && m.type.displayName === 'MessageContent')
-      console.log(Message)
-      patch('dababypng', Message, 'type', (_args, res) => {
+    const Message = getModule(m => m.type && m.type.displayName === 'MessageContent')
+    console.log(Message)
+    patch('dababypng', Message, 'type', (_args, res) => {
       const img = document.getElementsByTagName("img")
       for (let i = 0; i < img.length; i++) {
         const element = img[i];
@@ -54,25 +55,25 @@ export default class dababy extends Plugin {
       for (let i = 0; i < video.length; i++) {
         const element = video[i];
         if (element.getAttribute("src") != "https://cdn.discordapp.com/attachments/803993444917051404/825697846435119124/Dababy_car.mp4") {
-        element.setAttribute("src", "https://cdn.discordapp.com/attachments/803993444917051404/825697846435119124/Dababy_car.mp4")
+          element.setAttribute("src", "https://cdn.discordapp.com/attachments/803993444917051404/825697846435119124/Dababy_car.mp4")
         }
       }
-        res.props.children[0] = "lesgooo" 
-        const username = document.getElementsByClassName("username-1A8OIy")
-        for (let i = 0; i < username.length; i++) {
-          const element = username[i];
-          element.innerHTML = "DaBaby"
-        }
-		  const audio = document.addEventListener("click",         function play() {
+      res.props.children[0] = "lesgooo"
+      const username = document.getElementsByClassName("username-1A8OIy")
+      for (let i = 0; i < username.length; i++) {
+        const element = username[i];
+        element.innerHTML = "DaBaby"
+      }
+      const audio = document.addEventListener("click", function play() {
         var audio = new Audio('https://cdn.discordapp.com/attachments/803993444917051404/825720198627459092/lesgoo.mp3');
         audio.play();
-          })
-        return res;
       })
+      return res;
+    })
   }
 
-  stop () {
+  stop() {
     unpatch('dababypng');
-    setTimeout(() => window.location.reload(),2000)
+    setTimeout(() => window.location.reload(), 2000)
   }
 }
